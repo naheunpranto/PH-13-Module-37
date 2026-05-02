387e6309e55e880a456aa18abe748e8fe60c64a4
Naheun Alam <naheunpranto258@gmail.com>
2026-05-02 20:29:36 +0600

    Explore Dynamic Route , Params & load Data based on dynamic route

--- src/components/UserDetails/UserDetails.jsx
+++ src/components/UserDetails/UserDetails.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UserDetails = () => {
+
+    const user = useLoaderData();
+
+    const {name, website} = user
+    
+
     return (
         <div>
             <h3>User Details here</h3>
+            <h5>Name: {name}</h5>
+            <p>Website: {website}</p>
         </div>
     );
 };
